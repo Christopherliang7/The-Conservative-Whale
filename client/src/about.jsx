9f1a9352d0a2831a8eddbecfad96031518840769
Christopher <Christopherliang7@gmail.com>
2020-12-16 21:03:32 -0500

    Created CSS for Sightings Component for Demo

--- client/src/about.jsx
+++ client/src/about.jsx
@@ -5,7 +5,7 @@ function About() {
     <>
       <div className='about_container'>
         <h1 className='about_title'>About Us</h1>
-        <img className='about_picture' src='https://cdn2.wanderlust.co.uk/media/1073/shutterstock_776180275.jpg?anchor=center&mode=crop&width=600&height=225&format=auto&quality=30&rnd=132030060350000000' alt=''/>
+        <img className='about_picture' src='https://orcawhalewatch.com/wp-content/uploads/2016/03/whale-watching.jpg' alt=''/>
         <p className='about_description'>At The Conservative Whale, we believe in the importance of conservation and the impacts it can have on our future. We bring quality sighting information by scientists and allow you to post your own as well. We hope this platform continues to build, cultivating a growing community of like-minded individuals and spreading awareness of conservation as a whole. </p>
       </div>
       <br></br>
